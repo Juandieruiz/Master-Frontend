4b13784b304c1c32598f943eb8056a78bef8f909
Juan Diego Gómez <77864382+Juandieruiz@users.noreply.github.com>
2022-02-01 18:40:40 +0100

    Css listado articulos

--- Layout/main.js
+++ Layout/main.js
@@ -4,8 +4,8 @@ window.addEventListener("load", function() {
 
     for(let i = 1; i <= 5; i++){
     let clonado = template.cloneNode(true);
+        clonado.removeAttribute('id');
     let h2 = clonado.getElementsByTagName('h2')[0];
-    console.log(h2);
     h2.innerHTML = h2.textContent + ' ' + i;
 
         articles.appendChild(clonado);
